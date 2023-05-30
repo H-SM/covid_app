95c0700e72d2295a95615092883f35d92fae007c
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-05-30 09:59:00 +0530

    covid_app_commit

---
+++ index.js
@@ -0,0 +1,47 @@
+fetch("/data.json")
+    .then(response => response.json())
+    .then(rsp =>{
+        console.log(rsp.data);
+        rsp.data.forEach(element => {
+            // lat = element.latitude;
+            // long = element.longitude;
+            const { latitude, longitude, infected, name, country, dead, sick } = element;
+            // cases = element.infected;
+
+            if(infected>255)
+                color= "rgb(255,0,0)";
+            else 
+                color= `rgb(${infected},0,0)`;
+            
+            //mark on the map
+            const marker =new mapboxgl.Marker({
+                draggable: false,
+                color: color
+            }).setLngLat([longitude, latitude])
+            .addTo(map);
+
+            const popup = new mapboxgl.Popup({
+                closeButton: false,
+                closeOnClick: false
+            });
+
+            marker.getElement().addEventListener('mouseenter', () => {
+                popup.setLngLat(marker.getLngLat())
+                    .setHTML(
+                        `<h3>${name}</h3>` +
+                        `<p>Country: ${country}</p>` +
+                        `<p>Dead: ${dead}</p>` +
+                        `<p>Sick: ${sick}</p>`
+                    )
+                    .addTo(map);
+            });
+
+            marker.getElement().addEventListener('mouseleave', () => {
+                popup.remove();
+            });
+
+
+        });
+});
+
+//we can do a updateMap over a interval setInterval(updateMap ,20000) every 20 sec reload , this is done over an api, whcih shows the live count and showcase them
